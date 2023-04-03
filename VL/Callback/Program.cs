3cab86351659896f5b499d52bc4f19bae12246f0
Nick Häcker <nick.athaeck@googlemail.com>
2023-04-03 11:39:25 +0200

    .

---
+++ VL/Callback/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+
+namespace Callback // Note: actual namespace depends on the project name.
+{
+
+    // public interface IProgressReporter
+    // {
+    //     void ReportProgress(int percentDone);
+    // }
+
+
+    public delegate void ProgressReporter(int percentDone);
+
+
+    public static class Calculator
+    {
+        public static int SomeLengthyCalculation(ProgressReporter p)
+        {
+            for (int i = 0; i < 100; i++)
+            {
+                Thread.Sleep(100);
+                // Console.WriteLine()
+                p(i + 1);
+            }
+            return 42;
+        }
+
+    }
+✔
+    // class ProgressReporter : IProgressReporter
+    // {
+    //     public void ReportProgress(int percentDone)
+    //         Console.WriteLine(percentDone);
+    //     }
+    // }
+
+
+    internal class Program
+    {
+        public static void ReportProgress(int percentDone)
+        {
+            Console.WriteLine(percentDone);
+        }
+        static void Main(string[] args)
+        {
+            // ProgressReporter p = new ProgressReporter();
+            Console.WriteLine("Hello World!");
+            int result = Calculator.SomeLengthyCalculation(ReportProgress);
+            Console.WriteLine(result);
+        }
+    }
+}
--- VL/Vererbung/Program.cs
+++ VL/Vererbung/Program.cs
@@ -12,12 +12,106 @@ new Square { Height = 4, Width = 7 },
 new Circle { Radius = 7 },
 new Square { Height = 1, Width = 3 }
 };
-
+// DateTimeOffset now = DateTimeOffset.Now;
+// long timestampInSeconds = now.ToUnixTimeMilliseconds();
 foreach (TwoDObject t in some2DObjects)
 {
-    Console.WriteLine(t.Area());
+    // Console.WriteLine(t.Area());
+}
+// DateTimeOffset end = DateTimeOffset.Now;
+// long endTime = end.ToUnixTimeMilliseconds();
+
+// Console.WriteLine(endTime - timestampInSeconds);
+Object[] someObjects = {
+    new SimpleCircle{Radius=2},
+new SimpleSquare { Height = 4, Width = 7 },
+new SimpleCircle { Radius = 7 },
+new SimpleSquare { Height = 1, Width = 3 }
+};
+// DateTimeOffset now2 = DateTimeOffset.Now;
+// long timestampInSeconds2 = now.ToUnixTimeMilliseconds();
+foreach (Object o in someObjects)
+{
+
+    double area = o switch
+    {
+        SimpleCircle c => c.Radius * c.Radius * Math.PI,
+        SimpleSquare s => s.Height * s.Width,
+        _ => 0
+    };
+
+
+
+
+    //     // Console.WriteLine(t.Area());
+    //     double area;
+
+
+    //     switch (o)
+    //     {
+    //         case SimpleCircle sC:
+    //             area = sC.Radius * sC.Radius * Math.PI;
+    //             break;
+    //         case SimpleSquare sQ:
+    //             area = sQ.Height * sQ.Width;
+    //             break;
+    //         default:
+    //             area = 0;
+    //             break;
+    //     }
+
+
+
+    // if (o is SimpleCircle)
+    // {
+    //     SimpleCircle sc = (SimpleCircle)o;
+    //     // SimpleCircle? sc = o as SimpleCircle;
+    //     // if (sc == null)
+    //     // {
+    //     //     return;
+    //     // }
+    //     area = sc.Radius * sc.Radius * Math.PI;
+    // }
+    // else if (o is SimpleSquare)
+    // {
+    //     SimpleSquare sq = (SimpleSquare)o;
+    //     area = sq.Height * sq.Width;
+    // }
+    // else
+    // {
+    //     area = 0;
+    // }
+
+    // SimpleCircle? sc2 = o as SimpleCircle;
+    // if (sc2 != null)
+    // {
+    //     area = sc2.Radius * sc2.Radius * Math.PI;
+    // }
+    // Console.WriteLine(area);
+}
+// DateTimeOffset end2 = DateTimeOffset.Now;
+// long endTime2 = end.ToUnixTimeMilliseconds();
+// // 
+// Console.WriteLine(endTime2 - timestampInSeconds2);
+class SimpleCircle
+{
+    public double Radius;
 }
 
+class SimpleSquare
+{
+    public double Width;
+    public double Height;
+
+}
+
+
+
+
+
+
+
+// 
 
 class A
 {
