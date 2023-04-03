f34c41216192921eb3903173466a8a42d012556c
Nick Häcker <nick.athaeck@googlemail.com>
2023-04-03 12:52:38 +0200

    vl 03.04.2023

--- VL/Callback/Program.cs
+++ VL/Callback/Program.cs
@@ -10,23 +10,31 @@ namespace Callback // Note: actual namespace depends on the project name.
 
 
     public delegate void ProgressReporter(int percentDone);
+    public delegate void ResultReceiver(int result);
 
-
-    public static class Calculator
+    public class Calculator
     {
-        public static int SomeLengthyCalculation(ProgressReporter p)
+        public event ProgressReporter PR;
+        public event ResultReceiver RR;
+        public void SomeLengthyCalculation()
+        {
+            new Task(DoCalculate).Start();
+        }
+        private void DoCalculate()
         {
             for (int i = 0; i < 100; i++)
             {
                 Thread.Sleep(100);
                 // Console.WriteLine()
-                p(i + 1);
+                // p(i + 1);
+                PR(i + 1);
             }
-            return 42;
+            // return 42;
+            RR(42);
         }
 
     }
-✔
+
     // class ProgressReporter : IProgressReporter
     // {
     //     public void ReportProgress(int percentDone)
@@ -41,12 +49,35 @@ namespace Callback // Note: actual namespace depends on the project name.
         {
             Console.WriteLine(percentDone);
         }
+        public static void ResultReceiver(int percentDone)
+        {
+            Console.WriteLine(percentDone);
+        }
         static void Main(string[] args)
         {
             // ProgressReporter p = new ProgressReporter();
             Console.WriteLine("Hello World!");
-            int result = Calculator.SomeLengthyCalculation(ReportProgress);
-            Console.WriteLine(result);
+
+
+            // int result = Calculator.SomeLengthyCalculation(ReportProgress);
+
+
+            // Calculator calculator = new Calculator { PR = ReportProgress, RR = ResultReceiver };
+            Calculator calculator = new Calculator();
+            calculator.PR += ReportProgress;
+            calculator.PR += (percent) =>
+            {
+                Console.WriteLine("hallo");
+                Console.WriteLine(percent);
+            };
+
+            // Closure
+
+            calculator.RR += ResultReceiver;
+            calculator.SomeLengthyCalculation();
+            Thread.Sleep(20000);
+
+            // Console.WriteLine(result);
         }
     }
 }
