d4e9d633128d03186f53640401a6241c3459e522
Nick Häcker <nick.athaeck@googlemail.com>
2023-05-22 17:16:14 +0200

    vl 22.05.2023

--- VL/Blitter/Image/Program.cs
+++ VL/Blitter/Image/Program.cs
@@ -5,8 +5,15 @@ using System.Drawing.Imaging;
 Console.WriteLine("Hello, World!");
 
 Image image = new Image("./img/hfu.jpg");
+Image dest = new Image("./img/ibau_gross.jpg");
 Console.WriteLine(image);
-image.SaveAs("./img/test.jpg");
+
+image.Blit(0, 0, 200, 71, dest, 10, 10);
+
+dest.SaveAs("./img/test.jpg");
+
+
+
 public enum PixFormat
 {
     R8_G8_B8,
@@ -23,7 +30,7 @@ public class Image
     private byte[] _pixels;
     public int Width { init; get; }
     public int Height { init; get; }
-    PixFormat PixFormat;
+    public PixFormat PixFormat;
     int BytesPerPixel =>
     PixFormat switch
     {
@@ -37,17 +44,121 @@ public class Image
     };
 
 
-    private static void BlitCLip(ref int iSrc, ref int sizeBlock, ref int iDest, int sizeSrc, int sizeDest)
+    private static void BlitCLip(ref int iSrc, int sizeSrc, ref int sizeBlock, ref int iDest, int sizeDest)
     {
         int deltaMin = Math.Min(Math.Min(iSrc, iDest), 0);
 
         int deltaMax = Math.Max(Math.Max(iSrc + sizeBlock - sizeSrc, iDest + sizeBlock - sizeDest), 0);
 
-        // Wende durch Überlappung entstehende Werte (deltaMin/deltaMax) auf die "ref"-Werte an.
+        iSrc += deltaMin;
+        iDest += deltaMin;
+        sizeBlock += deltaMin;
+        sizeBlock -= deltaMax;
+
+        if (sizeBlock < 0)
+        {
+            sizeBlock = 0;
+        }
     }
+
+    delegate void CopyLineFunc(int iSrc, int Dst, int nPixels);
 
     public void Blit(int xs, int ys, int w, int h, Image dest, int xd, int yd)
     {
+        BlitCLip(ref xs, Width, ref w, ref xd, dest.Width);
+        BlitCLip(ref ys, Height, ref h, ref yd, dest.Height);
+
+        CopyLineFunc copyLine;
+
+        if (PixFormat == dest.PixFormat)
+        {
+            copyLine = (int iSrc, int iDest, int nPixels) =>
+            {
+                Array.Copy(_pixels, iSrc, dest._pixels, iDest, nPixels * BytesPerPixel);
+            };
+        }
+        else
+        {
+            switch (PixFormat)
+            {
+                case PixFormat.R8_G8_B8:
+                    switch (dest.PixFormat)
+                    {
+                        case PixFormat.R8_G8_B8:
+                            break;
+                        case PixFormat.R8_G8_B8_A8:
+                            copyLine = (int iSrc, int iDest, int nPixels) =>
+                            {
+                                for (int x = 0; x < nPixels; x++)
+                                {
+
+                                    // dest._pixels[iDest + x * dest.BytesPerPixel] = _pixels[iSrc + x * BytesPerPixel];
+                                    // dest._pixels[iDest + (x + 1) * dest.BytesPerPixel] = _pixels[iSrc + (x + 1) * BytesPerPixel];
+                                    // dest._pixels[iDest + (x + 2) * dest.BytesPerPixel] = _pixels[iSrc + (x + 2) * BytesPerPixel];
+                                    // dest._pixels[iDest + x * dest.BytesPerPixel] = _pixels[iSrc + x * BytesPerPixel];
+
+                                    // int iByteDest = 
+
+                                }
+                            };
+                            break;
+                        case PixFormat.I8:
+                            break;
+                    }
+                    break;
+                case PixFormat.R8_G8_B8_A8:
+                    switch (dest.PixFormat)
+                    {
+                        case PixFormat.R8_G8_B8:
+                            break;
+                        case PixFormat.R8_G8_B8_A8:
+                            break;
+                        case PixFormat.I8:
+                            break;
+                    }
+                    break;
+                case PixFormat.I8:
+                    switch (dest.PixFormat)
+                    {
+                        case PixFormat.R8_G8_B8:
+                            break;
+                        case PixFormat.R8_G8_B8_A8:
+                            break;
+                        case PixFormat.I8:
+                            break;
+                    }
+                    break;
+                default:
+                    break;
+            }
+        }
+
+        if (PixFormat != dest.PixFormat)
+        {
+            throw new Exception("Pixelformat does not match");
+        }
+
+        for (int y = 0; y < h; y++)
+        {
+            int iSrc = ((ys + y) * Width + xs) * BytesPerPixel;
+            int iDest = ((yd + y) * Width + xd) * dest.BytesPerPixel;
+
+            Array.Copy(_pixels, iSrc, dest._pixels, iDest, w * BytesPerPixel);
+
+            // for (int x = 0; x < w; x++)
+            // {
+            //     int iSrc = ((ys + y) * Width + xs + x) * BytesPerPixel;
+            //     int iDest = ((yd + y) * Width + xd + x) * dest.BytesPerPixel;
+            //     // _pixels[((ys + y) * Width + xs + x) * BytesPerPixel]
+
+            //     for (int b = 0; b < BytesPerPixel; b++)
+            //     {
+            //         dest._pixels[iDest + b] = _pixels[iSrc + b];
+            //     }
+
+            // }
+        }
+
 
     }
 
@@ -83,7 +194,7 @@ public class Image
                     for (int x = 0; x < Width; x++)
                     {
                         int pixIndex = (y * Width + x) * bpp;
-                        bm.SetPixel(x, y, Color.FromArgb(_pixels[pixIndex + 0], _pixels[pixIndex + 1], _pixels[pixIndex + 2], _pixels[pixIndex + 3]));
+                        bm.SetPixel(x, y, Color.FromArgb(_pixels[pixIndex + 3], _pixels[pixIndex + 0], _pixels[pixIndex + 1], _pixels[pixIndex + 2]));
                     }
                 }
                 break;
