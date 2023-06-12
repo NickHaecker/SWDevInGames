36f396685ef9f09ccb7e91cfc803b132ab2e3729
Nick Häcker <nick.athaeck@googlemail.com>
2023-06-12 15:32:25 +0200

    Vl 12.06.2023

---
+++ VL/Library/Class1.cs
@@ -0,0 +1,10 @@
+﻿namespace Library;
+public class Class1
+{
+    public void CallMe(int param1, string param2)
+    {
+        System.Console.WriteLine(param1 + param2);
+    }
+    public int IntField;
+    public string StrProp { get; set; } = "";
+}
