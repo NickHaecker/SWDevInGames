5c4191cedcbcab4f190ae278dece0ddbb94c5473
Nick Häcker <nick.athaeck@googlemail.com>
2023-06-26 17:15:07 +0200

    vl 26.06.2023

--- Prüfung/Program.cs
+++ Prüfung/Program.cs
@@ -1,105 +1,109 @@
-﻿UIElement dialog =
-new Group
-{
-    Name = "Ein Dialog",
-    Children = new List<UIElement>(new UIElement[] {
- new Label {Name="Label Eins", Contents="Hier steht was"},
- new Image {Name="Bild Eins", Path="Smiley.jpg", Width=400, Height=300},
- new TextInput {Name="Input", Text="Hier was eingeben" },
-})
-};
-SimpleTestVisitor v = new SimpleTestVisitor();
-dialog.Accept(v);
+﻿// UIElement dialog =
+// new Group
+// {
+//     Name = "Ein Dialog",
+//     Children = new List<UIElement>(new UIElement[] {
+//  new Label {Name="Label Eins", Contents="Hier steht was"},
+//  new Image {Name="Bild Eins", Path="Smiley.jpg", Width=400, Height=300},
+//  new TextInput {Name="Input", Text="Hier was eingeben" },
+// })
+// };
+// SimpleTestVisitor v = new SimpleTestVisitor();
+// dialog.Accept(v);
 
-// var i = "test";
+// // var i = "test";
 
-// i = 4;
+// // i = 4;
 
-// Console.WriteLine(i);
-
-
+// // Console.WriteLine(i);
 
 
 
 
+ int i = 42;
+  object o = i;
+  o = 43;
+  int j = (int)o;
+  Console.WriteLine("i is: " + i + "; o is: " + o + "; j is: " + j);
 
 
 
 
 
-public class UIElement
-{
-    public string Name = "";
-    public virtual void Accept(Visitor v) { }
-}
 
-public class Label : UIElement
-{
-    public string Contents = "";
-    public override void Accept(Visitor v) { v.Visit(this); }
-}
+// public class UIElement
+// {
+//     public string Name = "";
+//     public virtual void Accept(Visitor v) { }
+// }
 
-public class TextInput : UIElement
-{
-    public string Text = "";
-    public override void Accept(Visitor v) { v.Visit(this); }
-}
+// public class Label : UIElement
+// {
+//     public string Contents = "";
+//     public override void Accept(Visitor v) { v.Visit(this); }
+// }
 
-public class Image : UIElement
-{
-    public string? Path;
-    public int Width, Height;
-    public override void Accept(Visitor v) { v.Visit(this); }
-}
+// public class TextInput : UIElement
+// {
+//     public string Text = "";
+//     public override void Accept(Visitor v) { v.Visit(this); }
+// }
 
-public class Group : UIElement
-{
-    public List<UIElement> Children = new List<UIElement>();
+// public class Image : UIElement
+// {
+//     public string? Path;
+//     public int Width, Height;
+//     public override void Accept(Visitor v) { v.Visit(this); }
+// }
 
-    public override void Accept(Visitor v)
-    {
-        v.Visit(this);
-        if (Children != null)
-        {
-            foreach (UIElement uiElement in Children)
-            {
-                uiElement.Accept(v);
-            }
-        }
-    }
-}
+// public class Group : UIElement
+// {
+//     public List<UIElement> Children = new List<UIElement>();
 
-public class Visitor
-{
-    public virtual void Visit(UIElement e) { }
-    public virtual void Visit(Label l) { }
-    public virtual void Visit(TextInput t) { }
-    public virtual void Visit(Image i) { }
-    public virtual void Visit(Group g) { }
-}
+//     public override void Accept(Visitor v)
+//     {
+//         v.Visit(this);
+//         if (Children != null)
+//         {
+//             foreach (UIElement uiElement in Children)
+//             {
+//                 uiElement.Accept(v);
+//             }
+//         }
+//     }
+// }
 
-public class SimpleTestVisitor : Visitor
-{
-    public override void Visit(UIElement e)
-    {
-        Console.WriteLine("Unknown");
-    }
-    public override void Visit(Label l)
-    {
-        Console.WriteLine(l.Name + " is a Label: " + l.Contents);
-    }
-    public override void Visit(TextInput t)
-    {
-        Console.WriteLine(t.Name + " is a TextInput: " + t.Text);
-    }
-    public override void Visit(Image i)
-    {
-        Console.WriteLine(i.Name + " is an image. Size: (" + i.Width + "," + i.Height + ")");
-    }
-    public override void Visit(Group g)
-    {
-        Console.WriteLine(g.Name + " is a Group with " + g.Children.Count + " children.");
-    }
-}
+// public class Visitor
+// {
+//     public virtual void Visit(UIElement e) { }
+//     public virtual void Visit(Label l) { }
+//     public virtual void Visit(TextInput t) { }
+//     public virtual void Visit(Image i) { }
+//     public virtual void Visit(Group g) { }
+// }
+
+// public class SimpleTestVisitor : Visitor
+// {
+//     public override void Visit(UIElement e)
+//     {
+//         Console.WriteLine("Unknown");
+//     }
+//     public override void Visit(Label l)
+//     {
+//         Console.WriteLine(l.Name + " is a Label: " + l.Contents);
+//     }
+//     public override void Visit(TextInput t)
+//     {
+//         Console.WriteLine(t.Name + " is a TextInput: " + t.Text);
+//     }
+//     public override void Visit(Image i)
+//     {
+//         Console.WriteLine(i.Name + " is an image. Size: (" + i.Width + "," + i.Height + ")");
+//     }
+//     public override void Visit(Group g)
+//     {
+//         Console.WriteLine(g.Name + " is a Group with " + g.Children.Count + " children.");
+//     }
+// }
 
 
--- VL/Visitors/Program.cs
+++ VL/Visitors/Program.cs
@@ -1,17 +1,13 @@
-﻿
+﻿RenderVisitor v = new RenderVisitor();
 Group group = new Group
 {
     Name = "44",
     Children ={
-    new Sphere
-    { Name = "55", Radius = 3 },
-    new Group{
-        Name="qq",
-        Children ={
+    new Sphere{ Name = "55", Radius = 3 },
+    new Group{Name="qq",Children ={
     new Cuboid { Name = "11", Width = 1, Height = 1, Depth = 1 },
        new Cuboid { Name = "Just a cube", Width = 1, Height = 1, Depth = 1 },
             new Sphere { Name = "Sphere #2", Radius = 2 }
-
         }
     },
        new Cuboid { Name = "Just a cube", Width = 1, Height = 1, Depth = 1 }
@@ -20,13 +16,13 @@ Group group = new Group
 };
 
 
-group.Render();
+group.Accept(v);
 
 
 
 public abstract class GraphOb
 {
-    public abstract void Render();
+    public abstract void Accept(Visitor v);
     public string Name = "";
 }
 
@@ -34,9 +30,10 @@ public class Cuboid : GraphOb
 {
     public double Width; public double Height; public double Depth;
 
-    public override void Render()
+    public override void Accept(Visitor v)
     {
         Console.WriteLine("du humoid");
+        v.Visit(this);
     }
 }
 
@@ -45,25 +42,22 @@ public class Sphere : GraphOb
 {
     public double Radius;
 
-    public override void Render()
+    public override void Accept(Visitor v)
     {
-        Console.WriteLine("durch nudeln");
+        Console.WriteLine("durch nudeln"); v.Visit(this);
     }
 }
 public class Group : GraphOb
 {
     public List<GraphOb> Children = new List<GraphOb>();
 
-    public override void Render()
+    public override void Accept(Visitor v)
     {
+        v.Visit(this);
         Console.WriteLine("was machen sachen");
-        Iterate();
-    }
-    private void Iterate()
-    {
         foreach (GraphOb go in Children)
         {
-            go.Render();
+            go.Accept(v);
         }
     }
 }
@@ -73,22 +67,28 @@ public interface Visitor
     public void Visit(Sphere s);
     public void Visit(Cuboid c);
     public void Visit(Group g);
+    public void Visit(GraphOb g);
 }
 
 public class RenderVisitor : Visitor
 {
     public void Visit(Sphere s)
     {
-        throw new NotImplementedException();
+        Console.WriteLine(s.Name + s.Radius);
     }
 
     public void Visit(Cuboid c)
     {
-        throw new NotImplementedException();
+        Console.WriteLine(c.Height + c.Name + c.Width + c.Height);
     }
 
     public void Visit(Group g)
     {
-        throw new NotImplementedException();
+        Console.WriteLine(g.Name + g.Children.Count);
+    }
+    public void Visit(GraphOb g)
+    {
+        Console.WriteLine(g.Name);
+        // g.Accept(this);
     }
 }
