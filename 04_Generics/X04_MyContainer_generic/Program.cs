66ad0870953e28a671f6ed1c1427eece2e894702
Nick Häcker <nick.athaeck@googlemail.com>
2023-07-10 17:34:44 +0200

    tested something

--- 04_Generics/X04_MyContainer_generic/Program.cs
+++ 04_Generics/X04_MyContainer_generic/Program.cs
@@ -7,41 +7,40 @@ using System.Threading.Tasks;
 
 namespace X04_MyContainer_generic
 {
-   public class MyContainer<T> : IEnumerable<T>
+    public class MyContainer<T>
     {
         private T[] _theObjects;
         private int _n;
 
+        public T this[int i]
+        {
+            get { return _theObjects[i]; }
+            set { _theObjects[i] = value; }
+        }
         public MyContainer()
         {
             _theObjects = new T[2];
             _n = 0;
         }
-
         public void Add(T o)
         {
-            // If necessary, grow the array
             if (_n == _theObjects.Length)
             {
                 T[] oldArray = _theObjects;
                 _theObjects = new T[2 * oldArray.Length];
                 Array.Copy(oldArray, _theObjects, _n);
             }
-
             _theObjects[_n] = o;
             _n++;
         }
-
         public T GetAt(int i)
         {
             return _theObjects[i];
         }
-
         public int Count
         {
             get { return _n; }
         }
-
         public IEnumerator<T> GetEnumerator()
         {
             for (int i = 0; i < _n; i++)
@@ -49,29 +48,25 @@ namespace X04_MyContainer_generic
                 yield return _theObjects[i];
             }
         }
-
-        IEnumerator IEnumerable.GetEnumerator()
-        {
-            return GetEnumerator();
-        }
+        // IEnumerator IEnumerable.GetEnumerator()
+        // {
+        //     return GetEnumerator();
+        // }
     }
-
-
     class Program
     {
         static void Main(string[] args)
         {
             MyContainer<int> container = new MyContainer<int>();
-
             container.Add(3);
+            container[0] = 5;
             container.Add(2);
             container.Add(8);
             container.Add(8);
             container.Add(4);
-
-            for (int i = 0; i < container.Count; i++)
+            foreach (int i in container)
             {
-                Console.WriteLine($"Element at {i}: {container.GetAt(i)}");
+                Console.WriteLine($"Element: {i}");
             }
         }
     }
